6fea54630445684e0597acc0d91027ad92345ee9
RudiPachanin <rudi.pach@ya.ru>
2021-05-29 17:08:07 +0300

    complexity and endGame

--- src/Game.java
+++ src/Game.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Game {
+    private static Scanner scanner = new Scanner(System.in);
 
     private static Integer repeats(int[] array, int x) {
         int repeats = 0;
@@ -13,10 +14,31 @@ public class Game {
         return repeats;
     }
 
-    private static int[] getRandomNumbers() {
-        int length = 4;
-        int[] randomArray = new int[length];
+    private static int complexity() {
+        while (true) {
+            System.out.println("Введите сложноть: 3,4,5");
+            int answer = scanner.nextInt();
+            if (answer > 2 && answer < 6) {
+                return answer;
+            } else {
+                System.out.println("неправильное число");
+            }
+        }
+    }
 
+    private static int[] getRandomNumbers() {
+        int complexity = complexity();
+        int length = 0;
+        if (complexity == 3) {
+            length = 3;
+        }
+        if (complexity == 4) {
+            length = 4;
+        }
+        if (complexity == 5) {
+            length = 5;
+        }
+        int[] randomArray = new int[length];
         for (int i = 0; i < length; i++) {
             randomArray[i] = (int) (Math.random() * (10) + 0);
             do {
@@ -27,19 +49,36 @@ public class Game {
     }
 
     public static void startGame() {
+
         int[] randomNumbers = getRandomNumbers();
+        int length = randomNumbers.length;
         while (true) {
-            System.out.println("Введите 4 уникальныйх числа: ");
-            Scanner scanner = new Scanner(System.in);
-            int[] userArray = new int[4];
-            String[] strings = scanner.next().split("");
-            for (int i = 0; i < 4; i++) {
-                try {
-                    userArray[i] = Integer.parseInt(strings[i]);
-                } catch (Exception exception) {
-                    System.out.println("Только уникальные числа");
-                    startGame();
+            int count = 0;
+            count++;
+            System.out.println("Попытка: " + count);
+            System.out.println("Если хотите сдаться, то напишите <сдаюсь> ");
+            String endGame = scanner.next();
+
+            if (endGame.equals("сдаюсь")) {
+                for (int i = 0; i < randomNumbers.length; i++) {
+                    System.out.print(randomNumbers[i] + "");
                 }
+                System.out.println("");
+                System.out.println("Колличество попыток: " + count);
+                return;
+            }
+
+
+            System.out.println("Введите " + length + " уникальныйх числа: ");
+            int[] userArray = new int[length];
+            String[] strings = scanner.next().split("");
+            try {
+                for (int i = 0; i < length; i++) {
+                    userArray[i] = Integer.parseInt(strings[i]);
+                }
+            } catch (Exception exception) {
+                System.out.println("Только уникальные числа");
+                startGame();
             }
             for (int number : userArray) {
                 if (repeats(userArray, number) >= 2) {
@@ -50,6 +89,7 @@ public class Game {
             searchCowAndBulls(randomNumbers, userArray);
             if (Arrays.equals(randomNumbers, userArray)) {
                 System.out.println("Вы ВЫИГРАЛИ!!!");
+                System.out.println("Колличество ваших попыток равно: " + count);
                 System.out.println("Хотите еще раз? Если да введите 1, Если нет то введите 2.");
                 int again = scanner.nextInt();
                 if (again == 2) {
@@ -57,6 +97,7 @@ public class Game {
                 }
             }
         }
+
     }
 
     private static void searchCowAndBulls(int[] randomArray, int[] userArray) {
